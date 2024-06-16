63d4053e7a936f7cda59bed39a552331d76a523e
gumnbum <1962901241@qq.com>
2024-06-16 20:53:29 +0800

    1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AProgramerWithoutGlasses/tool-project
+
+go 1.21.5
---
+++ tool/stringLen.go
@@ -0,0 +1,9 @@
+package tool
+
+func StringLen(str string) int {
+	strlen := 0
+	for _, _ = range str {
+		strlen++
+	}
+	return strlen
+}
